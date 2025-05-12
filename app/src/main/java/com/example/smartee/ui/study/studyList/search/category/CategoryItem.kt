5a5d6cd2c13bbb9027b2e5ef25c98b7b97c0404a
explodeTheComputer <dnfjddk2@gmail.com>
2025-05-12 22:05:00 +0900

    [스터디 목록] CategoryItem 및 StudyCategory 컴포넌트 개선: 그림자 및 정렬 추가

--- app/src/main/java/com/example/smartee/ui/study/studyList/search/category/CategoryItem.kt
+++ app/src/main/java/com/example/smartee/ui/study/studyList/search/category/CategoryItem.kt
@@ -4,7 +4,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun CategoryItem(
@@ -12,8 +15,13 @@ fun CategoryItem(
     category: String,
     onClick: () -> Unit
 ) {
-    Box(modifier.clickable(onClick = onClick)
+    Box(
+        modifier.clickable(onClick = onClick),
+        contentAlignment = Alignment.Center
     ) {
-        Text(category)
+        Text(
+            category,
+            modifier = modifier.shadow(10.dp)
+            )
     }
 }
