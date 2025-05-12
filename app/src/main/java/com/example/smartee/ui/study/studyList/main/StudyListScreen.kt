de37b6b61ea238ef13f1df7ed0ca9bdc9aad796b
explodeTheComputer <dnfjddk2@gmail.com>
2025-05-13 01:55:33 +0900

    [스터디 목록] Pull to Refresh 기능 제거 아직 앱 중단

--- app/src/main/java/com/example/smartee/ui/study/studyList/main/StudyListScreen.kt
+++ app/src/main/java/com/example/smartee/ui/study/studyList/main/StudyListScreen.kt
@@ -1,22 +1,15 @@
 package com.example.smartee.ui.study.studyList.main
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.pullrefresh.PullRefreshIndicator
-import androidx.compose.material.pullrefresh.pullRefresh
-import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.smartee.model.StudyData
 import com.example.smartee.ui.LocalNavGraphViewModelStoreOwner
 import com.example.smartee.ui.study.studyList.main.topbar.StudyListTopBar
 import com.example.smartee.viewmodel.StudyViewModel
-import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -26,12 +19,13 @@ fun StudyListScreen(
 ) {
     val studyViewModel: StudyViewModel =
         viewModel(viewModelStoreOwner = LocalNavGraphViewModelStoreOwner.current)
-    val isRefreshing = studyViewModel.isRefreshing
-    val swipeState = rememberSwipeRefreshState(isRefreshing)//새로고침 기능
 
     // LiveData를 Compose 상태로 변환
     val filteredStudyList = studyViewModel.filteredStudyList.observeAsState(initial = emptyList<StudyData>()).value
 
+//    val isRefreshing = studyViewModel.isRefreshing
+
+//val swipeState = rememberSwipeRefreshState(isRefreshing)//새로고침 기능
 //    SwipeRefresh(
 //        state = swipeState,
 //        onRefresh = { studyViewModel.refreshStudyList() }
@@ -52,11 +46,11 @@ fun StudyListScreen(
 //        }
 //    }
 
-    val pullRefreshState = rememberPullRefreshState(
-        refreshing = isRefreshing,
-        onRefresh = { studyViewModel.refreshStudyList() }
-    )
-    Box(Modifier.pullRefresh(pullRefreshState)) {
+//    val pullRefreshState = rememberPullRefreshState(
+//        refreshing = isRefreshing,
+//        onRefresh = { studyViewModel.refreshStudyList() }
+//    )
+//    Box(Modifier.pullRefresh(pullRefreshState)) {
         Column {
             StudyListTopBar(
                 onSearchNavigate = onSearchNavigate,
@@ -70,12 +64,12 @@ fun StudyListScreen(
                 onStudyDetailNavigate = onStudyDetailNavigate,
             )
         }
-        PullRefreshIndicator(
-            refreshing = isRefreshing,
-            state = pullRefreshState,
-            modifier = Modifier.align(Alignment.TopCenter)
-        )
-    }
+//        PullRefreshIndicator(
+//            refreshing = isRefreshing,
+//            state = pullRefreshState,
+//            modifier = Modifier.align(Alignment.TopCenter)
+//        )
+//    }
 
 }
 
