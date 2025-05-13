97a99b69c78ebd57496635f192c35454bd34ae65
hwisungkk <naatomkr@gmail.com>
2025-05-13 15:02:27 +0900

    save

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     implementation(libs.androidx.runtime.livedata)
     implementation(libs.androidx.material) // 새로고침 기능
 
+    implementation(libs.androidx.material)
+    implementation(libs.firebase.common.ktx) // 새로고침 기능
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
