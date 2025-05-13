22dd8c4461fffdabda02109b35e66067dfac0764
explodeTheComputer <dnfjddk2@gmail.com>
2025-05-13 10:30:06 +0900

    [스터디 목록] StudyData 모델에 필요한 필드 추가

--- app/src/main/java/com/example/smartee/model/StudyData.kt
+++ app/src/main/java/com/example/smartee/model/StudyData.kt
@@ -14,13 +14,27 @@ data class StudyData(
     //스터디 기간
     var startDate: String = "",
     var endDate: String = "",
+    //정기/비정기
+    var isRegular:Boolean,
+
     var currentMemberCount: Int = 0,
     var maxMemberCount: Int = 0,
     var isOffline: Boolean = true,
+    //잉크 조건
+    var minInkLevel: Int = 0,
+    //차감 만년필
+    var penCount: Int = 0,
+    //벌칙
+    var punishment: String = "",
+    //스터디 설명
     var description: String = "반갑습니다 ㅎㅎ",
+
     var address: String = "",
+    //스터디 댓글
     var commentCount: Int = 0,
+    //스터디 좋아요
     var likeCount: Int = 0,
+    //스터디 썸네일
     var thumbnailModel: String = ""
 ) {
     // LocalDateTime 변환 헬퍼 함수
