c6f3f6261fda498eac6ea391ff04bcbc9fda7606
explodeTheComputer <dnfjddk2@gmail.com>
2025-05-16 22:39:25 +0900

    [스터디 검색] 불필요한 스크롤 기능 제거

--- app/src/main/java/com/example/smartee/ui/study/studyList/search/StudySearchScreen.kt
+++ app/src/main/java/com/example/smartee/ui/study/studyList/search/StudySearchScreen.kt
@@ -6,8 +6,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.TabRowDefaults.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -32,7 +30,6 @@ fun StudySearchScreen(
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .verticalScroll(rememberScrollState())
             .padding(16.dp)
     ) {
         // 메인 검색바
